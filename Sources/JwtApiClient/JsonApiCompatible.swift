cc6242ac11e16c1891021f7838465a8af0aee117
Chris Kobrzak <chris.kobrzak@gmail.com>
2022-03-07 15:37:13 +0000

    Favour API class instead of protocol with default methods
    This is for easier mocking with Mockingbird (it's also considered a
    a better pattern).

--- Sources/JwtApiClient/JsonApiCompatible.swift
+++ Sources/JwtApiClient/JsonApiCompatible.swift
@@ -20,29 +20,3 @@ public protocol JsonApiCompatible {
 
   func patch(_ url: URL!, token: String, dictionary: [String: Any]) async throws
 }
-
-public extension JsonApiCompatible {
-  func post<T>(_ url: URL!, _ dictionary: [String: Any]) async throws -> T where T : Decodable {
-    try await postJsonDictionary(url, dictionary)
-  }
-
-  func post<T>(_ url: URL!, token: String, dictionary: [String : Any]) async throws -> T where T : Decodable {
-    try await postJsonDictionaryWithToken(url, token: token, dictionary: dictionary)
-  }
-
-  func get<T>(_ url: URL, token: String) async throws -> T where T : Decodable {
-    try await getJsonWithToken(url, token: token)
-  }
-
-  func get<T>(_ endpoint: String, token: String) async throws -> T where T : Decodable {
-    try await getJsonWithToken(endpoint, token: token)
-  }
-
-  func delete(_ url: URL, token: String) async throws {
-    try await deleteWithToken(url, token: token)
-  }
-
-  func patch(_ url: URL!, token: String, dictionary: [String : Any]) async throws {
-    try await patchJsonDictionaryWithToken(url, token: token, dictionary: dictionary)
-  }
-}
